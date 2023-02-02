c19e68145b7aca4262e95fca79bfb9b2d68597d4
Thea <tonynl@student.uia.no>
2023-02-02 18:19:14 +0100

    endringer
    endringer

---
+++ fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
@@ -0,0 +1,13 @@
+package fib
+
+import "testing"
+
+func TestFib(t *testing.T) {
+	// Her går testen
+	input := 0
+	got := Fib(input)
+	want := 1
+	if got != want {
+		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+	}
+}
