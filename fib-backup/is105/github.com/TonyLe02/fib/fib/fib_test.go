465df7c61fc7f6e3ff7e2a7fd61607cef7a69de9
Thea <tonynl@student.uia.no>
2023-02-02 18:29:37 +0100

    e
    e

--- fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
+++ fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestFib(t *testing.T) {
 	// Her går testen
-	input := 0
-	got := Fib(input)
-	want := 1
+	input = 1 // legg merke til operatøren =
+	got = Fib(input)
+	want = 1
 	if got != want {
 		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
 	}
