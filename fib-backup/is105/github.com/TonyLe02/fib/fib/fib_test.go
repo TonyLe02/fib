71be0735a46d15f9eda0c9804624141137bae598
Thea <tonynl@student.uia.no>
2023-02-02 18:45:13 +0100

    Update fib_test.go

--- fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
+++ fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
@@ -4,10 +4,22 @@ import "testing"
 
 func TestFib(t *testing.T) {
 	// Her går testen
-	input = 1 // legg merke til operatøren =
-	got = Fib(input)
-	want = 1
-	if got != want {
-		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+	type test struct {
+		input int
+		want  int
+	}
+
+	var tests = []test{
+		{0, 0},
+		{1, 1},
+		// her kan vi bare legge på nye test cases
+	}
+
+	// tc - test case
+	for _, tc := range tests {
+		got := Fib(tc.input)
+		if got != tc.want {
+			t.Errorf("Fib(%d): want %d, got %d", tc.input, tc.want, got)
+		}
 	}
 }
