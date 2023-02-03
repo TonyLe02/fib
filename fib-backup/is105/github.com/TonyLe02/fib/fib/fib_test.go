df0791497ae088730e67e516554dc5d88dd7e535
Thea <tonynl@student.uia.no>
2023-02-03 08:36:48 +0100

    Lagt til flere FIB(n).

--- fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
+++ fib-backup/is105/github.com/TonyLe02/fib/fib/fib_test.go
@@ -10,7 +10,15 @@ func TestFib(t *testing.T) {
 	}
 
 	var tests = []test{
+		{3, 1},
+		{4, 1},
+		{5, 1},
+		{6, 1},
+		{7, 1},
+		{8, 1},
+		{9, 1},
 		{10, 1},
+		{11, 1},
 		// her kan vi bare legge på nye test cases
 	}
 
